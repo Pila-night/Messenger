70136fd400205312e63cbb482c3bd8c71146290a
Pila-night <uhfxtd99@gmail.com>
2025-06-01 20:16:15 +0300

    Разнес облъявления и реализацию по разным файлам. Добавлен новый тип, отвечающий за сообщение с солью. Нужен, чтобы не парсить строку сервер респонсе. Или может  переделать сервер респонс чтобы от него наследовать типы ответов. Пока думаю

--- Client/protocol.cpp
+++ Client/protocol.cpp
@@ -1,4 +1,218 @@
-/*#include "protocol.h"
+#include "protocol.h"
+#include "ByteBuffer.h"
+#include <QDebug>
 
-Protocol::Protocol() {}
-*/
+/*подсчет контрольной суммы.*/
+QByteArray Packet::crc(const ByteBuffer& buffer) const {
+    uint32_t crc = CRC::Calculate(buffer.constData(), buffer.size(), CRC::CRC_32());
+    ByteBuffer result;
+    result.writeIntLE(crc);
+    return result;
+}
+
+/*Получается пакет вида [Длина, которую необходимо прочитать][Тип][Длина][Данные][Длина][Данные][CRC]*/
+QByteArray Packet::serialize() const {
+    ByteBuffer buf;
+    buf.writeByte(static_cast<qint8>(getType()));
+
+
+    /*Получаем  [ТИП][Длина][Данные][Длина][Данные]*/
+    serializeData(buf);
+
+    QByteArray CRC  =  crc(buf);
+    buf.write(CRC);
+
+    /*Теперь  [ТИП][Длина][Данные][Длина][Данные][CRC]*/
+    ByteBuffer finalPacket;
+
+
+    finalPacket.writeIntLE(buf.length());
+    finalPacket.write(buf.readEnd());
+
+    return finalPacket;
+}
+
+void Packet::serializeString(ByteBuffer& buffer, const QString& str) {
+    QByteArray utf8 = str.toUtf8();
+    buffer.writeShortLE(utf8.size());
+    buffer.write(utf8);
+}
+
+QString Packet::deserializeString(ByteBuffer& buffer) {
+    qint16 length = buffer.readShortLE();
+    QByteArray data = buffer.read(length);
+    return QString::fromUtf8(data);
+}
+
+std::shared_ptr<Packet> Packet::deserialize(const QByteArray& data) {
+    ByteBuffer buffer(data);
+
+    qint32 totalLength = buffer.readIntLE();
+
+    qint8 typeValue = buffer.readByte();
+    PacketType type = static_cast<PacketType>(typeValue);
+
+    std::shared_ptr<Packet> packet;
+    switch (type) {
+    case PacketType::Register:
+        packet = std::make_shared<Register>();
+        break;
+    case PacketType::Auth:
+        packet = std::make_shared<Auth>();
+        break;
+    case PacketType::Message:
+        packet = std::make_shared<Message>();
+        break;
+    case PacketType::ServerResponse:
+        packet = std::make_shared<ServerResponse>();
+        break;
+    default:
+        return nullptr;
+    }
+
+    if (packet) {
+        packet->deserializeData(buffer);
+    }
+
+    return packet;
+}
+
+// --- Реализация класса Register ---
+
+void Register::serializeData(ByteBuffer& buffer) const {
+    Packet::serializeString(buffer, username);
+    Packet::serializeString(buffer, password);
+}
+
+void Register::deserializeData(ByteBuffer& buffer) {
+    username = Packet::deserializeString(buffer);
+    password = Packet::deserializeString(buffer);
+}
+
+PacketType Register::getType() const {
+    return PacketType::Register;
+}
+
+QString Register::getUsername() const {
+    return username;
+}
+
+void Register::setUsername(const QString &text) {
+    username = text;
+}
+
+QString Register::getPassword() const {
+    return password;
+}
+
+void Register::setPassword(const QString &text) {
+    password = text;
+}
+
+// --- Реализация класса Auth ---
+
+void Auth::serializeData(ByteBuffer& buffer) const {
+    Packet::serializeString(buffer, username);
+    Packet::serializeString(buffer, password);
+}
+
+void Auth::deserializeData(ByteBuffer& buffer) {
+    username = Packet::deserializeString(buffer);
+    password = Packet::deserializeString(buffer);
+}
+
+PacketType Auth::getType() const {
+    return PacketType::Auth;
+}
+
+QString Auth::getUsername() const {
+    return username;
+}
+
+void Auth::setUsername(const QString &text) {
+    username = text;
+}
+
+QString Auth::getPassword() const {
+    return password;
+}
+
+void Auth::setPassword(const QString &text) {
+    password = text;
+}
+
+// --- Реализация класса Message ---
+
+void Message::serializeData(ByteBuffer& buffer) const {
+    Packet::serializeString(buffer, from);
+    Packet::serializeString(buffer, text);
+}
+
+void Message::deserializeData(ByteBuffer& buffer) {
+    from = Packet::deserializeString(buffer);
+    text = Packet::deserializeString(buffer);
+}
+
+PacketType Message::getType() const {
+    return PacketType::Message;
+}
+
+QString Message::getFrom() const {
+    return from;
+}
+
+void Message::setFrom(const QString &text) {
+    from = text;
+}
+
+QString Message::getText() const {
+    return text;
+}
+
+void Message::setText(const QString &str) {
+    text = str;
+}
+
+// --- Реализация класса ServerResponse ---
+
+void ServerResponse::serializeData(ByteBuffer& buffer) const {
+    Packet::serializeString(buffer, message);
+}
+
+void ServerResponse::deserializeData(ByteBuffer& buffer) {
+    message = Packet::deserializeString(buffer);
+}
+
+PacketType ServerResponse::getType() const {
+    return PacketType::ServerResponse;
+}
+
+QString ServerResponse::getResponse() const {
+    return message;
+}
+
+void ServerResponse::SetResponse(const QString &text) {
+    message = text;
+}
+
+// --- AuthChalleng ---
+
+void AuthChallenge::serializeData(ByteBuffer& buffer) const {
+    Packet::serializeString(buffer, salt);
+}
+
+void AuthChallenge::deserializeData(ByteBuffer& buffer) {
+    salt = Packet::deserializeString(buffer);
+}
+
+PacketType AuthChallenge::getType() const {
+    return PacketType::AuthChallenge;
+}
+
+QString AuthChallenge::getSalt() const {
+    return salt;
+}
+
+void AuthChallenge::setSalt(const QString &s) {
+    salt = s;
+}
--- Client/protocol.h
+++ Client/protocol.h
@@ -4,20 +4,20 @@
 #include <memory>
 #include <QByteArray>
 #include <QIODevice>
+#include <QString>
 #include <CRC.h> // Библиотека с https://github.com/d-bahr/CRCpp
-#include <QDebug>
 #include "ByteBuffer.h"
 
 enum class PacketType : uint8_t {
     Register,
     Auth,
     Message,
-    ServerResponse
+    ServerResponse,
+    AuthChallenge
 };
 
 class Packet {
 protected:
-
     virtual void serializeData(ByteBuffer& buffer) const = 0;
     virtual void deserializeData(ByteBuffer& buffer) = 0;
 
@@ -25,9 +25,8 @@ protected:
     static QString deserializeString(ByteBuffer& buffer);
 
 public:
-
     QByteArray serialize() const;
-    static std::unique_ptr<Packet> deserialize(const QByteArray& data);
+    static std::shared_ptr<Packet> deserialize(const QByteArray& data);
 
     virtual PacketType getType() const = 0;
     virtual ~Packet() = default;
@@ -36,82 +35,21 @@ private:
     QByteArray crc(const ByteBuffer& buffer) const;
 };
 
-/*подсчет контрольной суммы.*/
-QByteArray Packet::crc(const ByteBuffer& buffer) const {
-    uint32_t crc = CRC::Calculate(buffer.constData(), buffer.size(), CRC::CRC_32());
-    ByteBuffer result;
-    result.writeIntLE(crc);
-    return result;
-}
-
-/*Получается пакет вида [Длина, которую необходимо прочитать][Тип][Длина][Данные][Длина][Данные][CRC]*/
-QByteArray Packet::serialize() const {
-    ByteBuffer buf;
-    buf.writeByte(static_cast<qint8>(getType()));
-
-
-    /*Получаем  [ТИП][Длина][Данные][Длина][Данные]*/
-    serializeData(buf);
-
-    QByteArray CRC  =  crc(buf);
-    buf.write(CRC);
-
-    /*Теперь  [ТИП][Длина][Данные][Длина][Данные][CRC]*/
-    ByteBuffer finalPacket;
-    finalPacket.writeIntLE(buf.length());
-    finalPacket.write(buf.readEnd());
-
-    return finalPacket;
-}
-
-
-
-void Packet::serializeString(ByteBuffer& buffer, const QString& str) {
-    QByteArray utf_8 = str.toUtf8();
-    buffer.writeShortLE(utf_8.size()); //записываю длину строки
-    buffer.write(utf_8); // записываю саму строку
-}
-
-QString Packet::deserializeString(ByteBuffer& buffer) {
-    qint16 length = buffer.readShortLE();
-    QByteArray data = buffer.read(length);
-    return QString::fromUtf8(data);
-}
-
-
 class Register : public Packet {
 private:
     QString username;
     QString password;
 
 protected:
-    void serializeData(ByteBuffer& buffer) const override {
-        serializeString(buffer, username);
-        serializeString(buffer, password);
-    }
-
-    void deserializeData(ByteBuffer& buffer) override {
-        username = deserializeString(buffer);
-        password = deserializeString(buffer);
-    }
+    void serializeData(ByteBuffer& buffer) const override;
+    void deserializeData(ByteBuffer& buffer) override;
 
 public:
-    PacketType getType() const override {
-        return PacketType::Register;
-    }
-    QString getUsername() const {
-        return username;
-    }
-    void setUsername(const QString & text){
-        username = text;
-    }
-
-    QString getPassword() const{
-        return password;
-    }
-    void setPassword(const QString& text) {
-        password = text;
-    }
+    PacketType getType() const override;
+    QString getUsername() const;
+    void setUsername(const QString &text);
+    QString getPassword() const;
+    void setPassword(const QString &text);
 };
 
 class Auth : public Packet {
@@ -120,33 +58,15 @@ private:
     QString password;
 
 protected:
-    void serializeData(ByteBuffer& buffer) const override {
-        serializeString(buffer, username);
-        serializeString(buffer, password);
-    }
-
-    void deserializeData(ByteBuffer& buffer) override {
-        username = deserializeString(buffer);
-        password = deserializeString(buffer);
-    }
+    void serializeData(ByteBuffer& buffer) const override;
+    void deserializeData(ByteBuffer& buffer) override;
 
 public:
-    PacketType getType() const override {
-        return PacketType::Auth;
-    }
-    QString getUsername() const {
-        return username;
-    }
-    void setUsername(const QString & text){
-        username = text;
-    }
-
-    QString getPassword() const{
-        return password;
-    }
-    void setPassword(const QString& text) {
-        password = text;
-    }
+    PacketType getType() const override;
+    QString getUsername() const;
+    void setUsername(const QString &text);
+    QString getPassword() const;
+    void setPassword(const QString &text);
 };
 
 class Message : public Packet {
@@ -155,33 +75,15 @@ private:
     QString text;
 
 protected:
-    void serializeData(ByteBuffer& buffer) const override {
-        serializeString(buffer, from);
-        serializeString(buffer, text);
-    }
-
-    void deserializeData(ByteBuffer& buffer) override {
-        from = deserializeString(buffer);
-        text = deserializeString(buffer);
-    }
+    void serializeData(ByteBuffer& buffer) const override;
+    void deserializeData(ByteBuffer& buffer) override;
 
 public:
-    PacketType getType() const override {
-        return PacketType::Message;
-    }
-    QString getFrom() const {
-        return from;
-    }
-    void setFrom(const QString &text) {
-        from = text;
-    }
-
-    QString getText() const {
-        return text;
-    }
-    void setText(const QString &str){
-        text = str;
-    }
+    PacketType getType() const override;
+    QString getFrom() const;
+    void setFrom(const QString &text);
+    QString getText() const;
+    void setText(const QString &str);
 };
 
 class ServerResponse : public Packet {
@@ -189,61 +91,25 @@ private:
     QString message;
 
 protected:
-    void serializeData(ByteBuffer& buffer) const override {
-        serializeString(buffer, message);
-    }
-
-    void deserializeData(ByteBuffer& buffer) override {
-        message = deserializeString(buffer);
-    }
+    void serializeData(ByteBuffer& buffer) const override;
+    void deserializeData(ByteBuffer& buffer) override;
 
 public:
-    PacketType getType() const override {
-        return PacketType::ServerResponse;
-    }
-    QString getResponse()  const {
-        return message;
-    }
-    void SetResponse(const QString &text) {
-        message = text;
-    }
+    PacketType getType() const override;
+    QString getResponse() const;
+    void SetResponse(const QString &text);
 };
 
+class AuthChallenge : public Packet {
+private:
+    QString salt;
 
+protected:
+    void serializeData(ByteBuffer& buffer) const override;
+    void deserializeData(ByteBuffer& buffer) override;
 
-std::unique_ptr<Packet> Packet::deserialize(const QByteArray& data) {
-    ByteBuffer buffer(data);
-
-    qint32 totalLength = buffer.readIntLE();
-
-    qint8 typeValue = buffer.readByte();
-
-    PacketType type = static_cast<PacketType>(typeValue);
-
-    std::unique_ptr<Packet> packet;
-    switch (type) {
-    case PacketType::Register: {
-        packet = std::make_unique<Register>();
-        break;
-    }
-    case PacketType::Auth: {
-        packet = std::make_unique<Auth>();
-        break;
-    }
-    case PacketType::Message: {
-        packet = std::make_unique<Message>();
-        break;
-    }
-    case PacketType::ServerResponse: {
-        packet = std::make_unique<ServerResponse>();
-        break;
-    }
-    default: return nullptr;
-    }
-
-    if (packet) {
-        packet->deserializeData(buffer);
-    }
-
-    return packet;
-}
+public:
+    PacketType getType() const override;
+    QString getSalt() const;
+    void setSalt(const QString &s);
+};
